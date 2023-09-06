c7cc8f7cdbe73fbe04d00bac73ad2ab018f76366
lavinas <lavinas@me.com>
2023-09-06 14:50:13 -0300

    last

--- src/client/api/new/test.py
+++ src/client/api/new/test.py
@@ -1,12 +1,18 @@
 import unittest
+from .... import get_app 
+from .control import NewClient
 
 class TestClientNew(unittest.TestCase):
     def test_run_ok(self):
-        self.assertTrue(True)
+        self.app.app_context()
+        self.control.run()
     
     def setUp(self):
-        pass
-    
+        self.app = get_app()
+        self.control = NewClient(Settings(), Db(), Log())
+
+
+# mocks
 class Settings(object):
     pass
 
